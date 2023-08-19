cefcdc9c4a47d6e82466208e8bc4703477c0e36b
ralex2304 <ralex2304@gmail.com>
2023-08-19 20:29:30 +0300

    Test mode added

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@
 #include <assert.h>
 
 /**
+ * @brief Enables test mode
+ */
+#define TEST
+#ifdef TEST
+    const char test_name[] = "tests/test.txt"; // test file name
+#endif
+
+/**
  * @brief Specifies number of soludions of the equation
  */
 enum SOLUTIONS {
@@ -37,8 +45,9 @@ SOLUTIONS solve_lin(SolverData* data);
 SOLUTIONS solve_quad(SolverData* data);
 void print_roots(const SolverData* data);
 
-int input_flush();
-int read_double(double* x);
+int input_flush(FILE* stream = stdin);
+int read_double(double* x, FILE* stream = stdin);
+int read_int(int* x, FILE* stream = stdin);
 int enter_coeff(const char c, double* x);
 /**
  * @brief Tells if two doubles are equal with precision of DOUBLE_COMPARE_PRECISION
@@ -54,16 +63,29 @@ inline int is_double_equal(const double a, const double b) {
     return abs(a - b) < DOUBLE_COMPARE_PREC;
 };
 
+#ifdef TEST
+    void test_open_file(FILE** file, const char* filename);
+    int test_enter_coeffs(SolverData* data, FILE* file);
+    void test_read_roots(const SolverData* data, SolverData* correc_data, FILE* file);
+    void test_check_data(const SolverData* data, const SolverData* correct_data);
+#endif
+
 int main() {
     printf("Hello! This program solves quad equations of the form a*x^2 + b*x + c = 0\n"
            "Where \"x\" is a variable and \"a\", \"b\", \"c\" are coefficients\n");
     
     // Read data
-    SolverData data;
-    for (int i = 0; i < SolverData::COEFF_NUM; i++)
+    SolverData data = {};
+    for (int i = 0; i < data.COEFF_NUM; i++)
         data.coeffs[i] = NAN;
-    if (!enter_coeffs(&data)) exit(0);
-    for (int i = 0; i < SolverData::COEFF_NUM; i++)
+    #ifndef TEST
+        if (!enter_coeffs(&data)) exit(0);
+    #else
+        FILE* file = NULL;
+        test_open_file(&file, test_name);
+        if (!test_enter_coeffs(&data, file)) exit(0);
+    #endif
+    for (int i = 0; i < data.COEFF_NUM; i++)
         assert(!isnan(data.coeffs[i]));
     
     // Solve data
@@ -72,7 +94,18 @@ int main() {
     data.roots_num = solve_quad(&data);
 
     // Write data
-    print_roots(&data);
+    #ifndef TEST
+        print_roots(&data);
+    #else
+        SolverData correct_data = {};
+        for (int i = 0; i < correct_data.COEFF_NUM - 1; i++)
+            correct_data.roots[i] = NAN;
+        test_read_roots(&data, &correct_data,file);
+        for (int i = 0; i < correct_data.roots_num; i++)
+            assert(!isnan(correct_data.roots[i]));
+        fclose(file);
+        test_check_data(&data, &correct_data);
+    #endif
     
     printf("Bye!");
     return 0;
@@ -83,12 +116,12 @@ int main() {
  * 
  * @return int number of flushed symbols
  */
-int input_flush() {
+int input_flush(FILE* stream) {
     int c = 0;
     int cnt = 0;
     do {
         cnt++;
-    } while ((c = getchar()) != '\n' && c != EOF);
+    } while ((c = fgetc(stream)) != '\n' && c != EOF && !feof(stream) && !ferror(stream));
     
     return cnt;
 }
@@ -97,14 +130,30 @@ int input_flush() {
 /**
  * @brief Reads double from input to pointer
  * 
- * @param[in] x 
+ * @param[in] x
+ * @param[in] stream 
  * @return int success
  */
-int read_double(double* x) {
+int read_double(double* x, FILE* stream) {
     assert(x);
 
-    int res = scanf("%lf", x);
-    return input_flush() == 1 && res == 1 && res != EOF && !isnan(*x) && !isinf(*x);    // check input errors and flush input
+    int res = fscanf(stream, "%lf", x);
+    return input_flush(stream) == 1 && res == 1 && res != EOF && !isnan(*x) && !isinf(*x);    // check input errors and flush input
+}
+
+// 
+/**
+ * @brief Reads int from input to pointer
+ * 
+ * @param[in] x 
+ * @param[in] stream 
+ * @return int success
+ */
+int read_int(int* x, FILE* stream) {
+    assert(x);
+
+    int res = fscanf(stream, "%d", x);
+    return input_flush(stream) == 1 && res == 1 && res != EOF && !isnan(*x) && !isinf(*x);    // check input errors and flush input
 }
 
 // 
@@ -225,3 +274,98 @@ void print_roots(const SolverData* data){
             break;
     }
 }
+
+#ifdef TEST
+    /**
+     * @brief Opens test file by filename
+     * 
+     * @param[in] file - pointer 
+     * @param[in] filename - name of file
+     */
+    void test_open_file(FILE** file, const char* filename) {
+        *file = fopen(filename, "r");
+        if (*file == NULL) {
+            printf("Error opening test file %s\n", filename);
+            fclose(*file);
+            exit(0);
+        }
+    }
+
+    /**
+     * @brief Reads coeffs from test file
+     * 
+     * @param[in] data 
+     * @param[in] file 
+     * @return int success
+     */
+    int test_enter_coeffs(SolverData* data, FILE* file) {
+        assert(data && file);
+
+        for (int i = 0; i < data->COEFF_NUM; i++) {
+            if (!read_double(data->coeffs + i, file)) {
+                printf("Error reading coefficient from test file\n");
+                fclose(file);
+                return 0;
+            } 
+        }
+        return 1;
+    }
+
+    /**
+     * @brief Reads correct roots from test file
+     * 
+     * @param[in] data 
+     * @param[in] correct_data 
+     * @param[in] file 
+     */
+    void test_read_roots(const SolverData* data, SolverData* correct_data, FILE* file) {
+        assert(data && correct_data && file);
+        
+        if (!read_int(((int*)&correct_data->roots_num), file)) {
+            printf("Error reading number of solutions from test file\n");
+            fclose(file);
+            exit(0);
+        }
+
+        for (int i = 0; i < (int)correct_data->roots_num; i++) {
+            if(!read_double(&correct_data->roots[i], file)) {
+                printf("Error reading solution from test file\n");
+                fclose(file);
+                exit(0);
+            }
+        }
+    }
+
+    /**
+     * @brief Checks solutions number and roots
+     * 
+     * @param[in] data 
+     * @param[in] correct_data 
+     */
+    void test_check_data(const SolverData* data, const SolverData* correct_data) {
+        assert(data && correct_data);
+
+        if (data->roots_num != correct_data->roots_num) {
+            printf("Test failed. Incorrect number of solutions\n");
+            return;
+        } else {
+            printf("Number of solutions is correct: %d\n", (int)correct_data->roots_num);
+        }
+
+        // Тут конечно по-хорошему надо отсортировать корни, но это TODO
+        int success = 1;
+        for (int i = 0; i < (int) correct_data->roots_num; i++) {
+            if (!is_double_equal(data->roots[i], correct_data->roots[i])) {
+                printf("Test failed. Incorrect root %d\n", (i+1));
+                printf("Correct root:  %lf\n", correct_data->roots[i]);
+                printf("Received root: %lf\n" , data->roots[i]);
+                success = 0;
+            } else {
+                printf("Root %d is correct\n", (i+1));
+            }
+        }
+        if (!success) return;
+
+        printf("Test completed successfully!\n");
+    }
+#endif
